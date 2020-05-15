fccdb2b12b19bd52ab18c375f457df5a7e67ae7f
Vladimir <v_litva@mail.ru>
2020-05-15 19:00:16 +0300

    update 15.05.2020

--- ProPetsLostAndFound/src/main/java/propets/controller/lostandfound/LostAndFoundController.java
+++ ProPetsLostAndFound/src/main/java/propets/controller/lostandfound/LostAndFoundController.java
@@ -20,7 +20,7 @@ import propets.dto.lostandfound.LostFoundRequestDto;
 import propets.dto.lostandfound.LostFoundResponseDto;
 import propets.service.lostandfound.LostAndFoundService;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", exposedHeaders = "X-token")
 @RestController
 @RequestMapping("/{lang}/v1")
 public class LostAndFoundController {
