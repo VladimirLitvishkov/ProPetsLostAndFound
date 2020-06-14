219e758768f9de0523db603cf0a9ad2314809a01
Vladimir <v_litva@mail.ru>
2020-06-14 19:49:49 +0300

    update 14.06.2020

--- ProPetsLostAndFound/src/main/java/propets/model/lostandfound/Address.java
+++ ProPetsLostAndFound/src/main/java/propets/model/lostandfound/Address.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Builder
-@EqualsAndHashCode(of = "coordinates")
+@EqualsAndHashCode
 public class Address implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
